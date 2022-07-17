713ad96433c32458853c28a627b43eefa8431daa
PPhive <yw3726@nyu.edu>
2022-07-17 09:46:16 -0400

    Double edge

--- Assets/Scripts/Entities/Items/Items.cs
+++ Assets/Scripts/Entities/Items/Items.cs
@@ -62,7 +62,7 @@ public class Items : MonoBehaviour
             {
                 if (Player.Instance.FindClosestIdxFace(Vector3.up) == Player.OriginalNumberToIndex(1)) 
                 {
-                    Player.Instance.NextAtkMultiplier *=  2;
+                    Player.Instance.NextAtkMultiplier +=  2;
                 }
             }
         }
@@ -77,6 +77,14 @@ public class Items : MonoBehaviour
             if (CurrentState == ActivateStates.EndTurn) 
             {
                 Player.Instance.ChangeHPBy(-2);
+            }
+        }
+
+        if (Name == "Double Edge")
+        {
+            if (CurrentState == ActivateStates.Manual)
+            {
+                Player.Instance.NextAtkMultiplier += 1;
             }
         }
     }
