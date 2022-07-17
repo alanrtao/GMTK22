64d2ac47d572ac6cc4912ed4f84541a47ea0a927
PPhive <yw3726@nyu.edu>
2022-07-17 07:55:01 -0400

    Add HealthBar

---
+++ Assets/Scripts/Entities/HealthBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public BaseRollable MyMaster;
+    public bool FollowMaster;
+    public Vector3 InitialScale;
+    public Image MyBar;
+
+    void Start()
+    {
+        InitialScale = MyBar.rectTransform.localScale;
+    }
+
+    void Update()
+    {
+        Debug.Log(MyMaster.HP / MyMaster.MAX_HP);
+        MyBar.rectTransform.localScale = new Vector2((float)MyMaster.HP / (float)MyMaster.MAX_HP * InitialScale.x, InitialScale.y);
+    }
+}
